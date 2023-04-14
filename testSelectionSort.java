b7457dd27b1d5bee585444d0e3bb05044c31085f
Muhammad Firdaus Fadlizam <mfadliza@umich.edu>
2023-04-13 23:20:11 -0400

    Tom added testPositive and testNegative method

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -35,14 +35,35 @@ public class testSelectionSort {
 		Sortedarr[3] = 9;
 		Sortedarr[4] = 10;
 
+		SelectionSort selectionSort = new SelectionSort();
 
+		int[] afterSorted = selectionSort.basicSelectionSort(arr);
 
-
+		assertArrayEquals(Sortedarr, afterSorted);
 
 		/** add tests to check for this unit test **/
 	}
 
 	public void testNegative() {
+		
+		int[] arr = new int[5];
+		arr[0] = -1;
+		arr[1] = -2;
+		arr[2] = -100;
+		arr[3] = -82;
+		arr[4] = -20;
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = -100;
+		Sortedarr[1] = -82;
+		Sortedarr[2] = -20;
+		Sortedarr[3] = -2;
+		Sortedarr[4] = -1;
+
+		SelectionSort selectionSort = new SelectionSort();
+
+		int[] afterSorted = selectionSort.basicSelectionSort(arr);
+
+		assertArrayEquals(Sortedarr, afterSorted);
 
 		
 	}
